b11606cbe9f74431e4f59129886095961b9080a3
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-01-30 19:30:05 -0300

    barra de pesquisa

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import BarraDePesquisa from './components/BarraDePesquisa'
 import Sidebar from './components/Sidebar'
 
 function App () {
   return (
-    <Sidebar />
+    <>
+      <Sidebar />
+      <BarraDePesquisa />
+    </>
   )
 }
 
---
+++ src/components/BarraDePesquisa/index.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function BarraDePesquisa() {
+  return (
+    <input
+      type="search"
+      placeholder="Digite o que você procura"
+      className="barra-pesquisa"
+    />
+  );
+}
