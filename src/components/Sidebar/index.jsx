330e5ef5b337bf212423ba0dcff9652b2f8106a8
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-01-30 18:39:43 -0300

    estilizando sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -3,38 +3,40 @@ import Feed from "./assets/feed.svg";
 import Perfil from "./assets/account_circle.svg";
 import Info from "./assets/info.svg";
 import Logout from "./assets/logout.svg";
+import "./styles.css";
 
 export default function Sidebar() {
   return (
     <aside>
-      <img src={Logo} alt="Logo do codeconnect" />
-
+      <img src={Logo} alt="Logo do CodeConnect" />
       <nav>
-        <ul>
+        <ul className="lista-sidebar">
           <li>
-            <a href="#">Publicar</a>
+            <a href="#" className="item__link-publicacao">
+              Publicar
+            </a>
           </li>
           <li>
-            <a href="#">
-              <img src={Feed} alt="ícone de revista indicando feed" />
+            <a href="#" className="item__link item__link--ativo">
+              <img src={Feed} alt="" />
               <span>Feed</span>
             </a>
           </li>
           <li>
-            <a href="#">
-              <img src={Perfil} alt="ícone de perfil" />
+            <a href="#" className="item__link">
+              <img src={Perfil} alt="" />
               <span>Perfil</span>
             </a>
           </li>
           <li>
-            <a>
-              <img src={Info} alt="ícone de informações" />
+            <a href="#" className="item__link">
+              <img src={Info} alt="" />
               <span>Sobre nós</span>
             </a>
           </li>
           <li>
-            <a href="#">
-              <img src={Logout} />
+            <a href="#" className="item__link">
+              <img src={Logout} alt="" />
               <span>Sair</span>
             </a>
           </li>
