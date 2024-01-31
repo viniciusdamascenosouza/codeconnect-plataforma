94c3e0df08ac3d173daa41c019a5ea0e76678d18
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2024-01-31 08:07:48 -0300

    Criação e estilização do componente de filtros

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import BarraDePesquisa from "./components/BarraDePesquisa";
+import Filtro from "./components/Filtro";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="container">
       <Sidebar />
-      <BarraDePesquisa />
+      <div>
+        <BarraDePesquisa />
+        <Filtro />
+      </div>
     </div>
   );
 }
---
+++ src/components/Filtro/index.jsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function Filtro () {
+    return(
+        <section className="container-filtro">
+            <ul>
+                <li>Programação</li>
+                <li>React</li>
+            </ul>
+
+            <button>Limpar tudo</button>
+        </section>
+    )
+}
